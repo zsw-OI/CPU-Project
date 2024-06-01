3ac54885f9fb68c4e1ab5b9a997905e4bdf2c97c
Walnut <2124934587@qq.com>
2024-06-01 17:29:16 +0800

    fixed unaligned access

--- project_2.srcs/sources_1/new/memory.v
+++ project_2.srcs/sources_1/new/memory.v
@@ -40,9 +40,12 @@ module memory(
 `include "constants.v"
 
 wire [31:0] data;
+wire [1:0] offset;
 reg [31:0] data_w;
 wire uart_en;
+
 assign uart_en = uart_enable & (~uart_done);
+assign offset = addr[1:0];
 
 mem_block2 data_mem(
     .clka(uart_en ? ~uart_clk : ~clk), 
@@ -53,14 +56,37 @@ mem_block2 data_mem(
     .douta(data)
 );
 
+reg [7:0] byte_data;
+reg [31:0] byte_in;
+reg [15:0] hw_data;
+reg [31:0] hw_in;
+
 always @* begin
     res = 0;
+
+    case(offset)
+        2'b00: byte_data = data[7:0];
+        2'b01: byte_data = data[15:8];
+        2'b10: byte_data = data[23:16];
+        2'b11: byte_data = data[31:24];
+    endcase
+
+    case(offset)
+        2'b00: byte_in = {data[31:8], data_in[7:0]};
+        2'b01: byte_in = {data[31:16], data_in[15:8], data[7:0]};
+        2'b10: byte_in = {data[31:24], data_in[23:16], data[15:0]};
+        2'b11: byte_in = {data_in[31:24], data[23:0]};
+    endcase
+
+    hw_data = (offset[1] ? data[31:16] : data[15:0]);
+    hw_in = (offset[1]? ({data_in[31:16], data[15:0]}): ({data[31:16], data_in[15:0]}));
+
     case(mem_op)
-        MEM_LB: res = $signed(data[7:0]);
-        MEM_LH: res = $signed(data[15:0]);
+        MEM_LB: res = $signed(byte_data);
+        MEM_LH: res = $signed(hw_data);
         MEM_LW: res = data;
-        MEM_LBU:res = data[7:0]; 
-        MEM_LHU:res = data[15:0];
+        MEM_LBU:res = byte_data; 
+        MEM_LHU:res = hw_data;
         MEM_IN: res = input_data;
         default:res = 0;
     endcase
@@ -70,8 +96,8 @@ always @(*) begin
 //    output_flag = 0;
     data_w = 0;
     case(mem_op)
-        MEM_SB: data_w = {data[31:8], data_in[7:0]};
-        MEM_SH: data_w = {data[31:16], data_in[15:0]};
+        MEM_SB: data_w = byte_in;
+        MEM_SH: data_w = hw_in;
         MEM_SW: data_w = data_in;
         MEM_OUT:begin
             if(write_flag)begin
